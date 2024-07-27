80be6e3f07a1a107053410d4d0dac83c78e142f4
Louis <louis.driffort@gmail.com>
2024-07-27 18:07:22 +0200

    remove roomId from add members to room validator, use req params instead of body for room id

--- server/src/handlers/routes/rooms/routes.ts
+++ server/src/handlers/routes/rooms/routes.ts
@@ -1,7 +1,9 @@
 import express from "express"
 import { newRoomRoute } from "./newRoom"
+import { addMembersToRoomRoute } from "./addUsers"
 
 
 export const initRoomRoutes = (app : express.Express) => {
    newRoomRoute(app)
+   addMembersToRoomRoute(app)
 }
--- server/src/handlers/validators/rooms/add-members-validator.ts
+++ server/src/handlers/validators/rooms/add-members-validator.ts
@@ -2,12 +2,10 @@ import Joi from "joi";
 
 export const addMembersToRoomValidation = Joi.object<AddMembersToRoomRequest>({
 
-    roomId:    Joi.number().required(),
-    usernames: Joi.array().items(Joi.string().required()).min(1).required(),
+    usernames: Joi.array().items(Joi.string().required()).min(1).required()
 
 }).options({ abortEarly: false })
 
 export interface AddMembersToRoomRequest {
-    roomId: number
     usernames: string[]
 }
