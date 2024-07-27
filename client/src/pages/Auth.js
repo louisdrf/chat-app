5f66f9128b68ca7f8018273a8ff273c7117718ac
Louis <louis.driffort@gmail.com>
2024-07-27 14:28:59 +0200

    add lottie and style to auth page

--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -1,43 +1,61 @@
+import React, { useState } from 'react';
+import { LoginComponent } from '../components/LoginComponent';
+import { RegisterComponent } from '../components/RegisterComponent';
+import { Button, Row, Col, Typography } from 'antd';
+import Lottie from 'react-lottie';
+import loginAnimationData from '../assets/lotties/login.json';
+import '../styles/auth-page.scss';
 
-import React, { useState } from 'react'
-import { LoginComponent } from '../components/LoginComponent.js'
-import { RegisterComponent } from '../components/RegisterComponent.js'
-import { Button } from 'antd'
+const { Title, Paragraph } = Typography;
 
 export const Auth = () => {
+  const [showLoginForm, setShowLoginForm] = useState(true);
 
-    const [showLoginForm, setShowLoginForm] = useState(true)
+  const toggleForm = () => {
+    setShowLoginForm(!showLoginForm);
+  };
 
-    const toggleForm = () => {
-        setShowLoginForm(!showLoginForm)
-    }
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: loginAnimationData,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice',
+    },
+  };
 
-
-    return (
-        <div className="auth-page-container">
+  return (
+    <div className="auth-page">
+      <Row className="auth-container">
+        <Col span={12} className="auth-animation">
+          <Lottie options={defaultOptions} height={400} width={400} />
+        </Col>
+        <Col span={12} className="auth-form">
           {showLoginForm ? (
             <>
-              <h2>Connexion</h2>
+              <Title level={2}>Connexion</Title>
               <LoginComponent />
-              <p>
+              <Paragraph>
                 Je n'ai pas de compte ?{' '}
                 <Button type="link" onClick={toggleForm}>
                   Inscrivez-vous
                 </Button>
-              </p>
+              </Paragraph>
             </>
           ) : (
             <>
-              <h2>Inscription</h2>
+              <Title level={2}>Inscription</Title>
               <RegisterComponent />
-              <p>
+              <Paragraph>
                 J'ai déjà un compte ?{' '}
                 <Button type="link" onClick={toggleForm}>
                   Connectez-vous
                 </Button>
-              </p>
+              </Paragraph>
             </>
           )}
-        </div>
-      );
-}
+        </Col>
+      </Row>
+    </div>
+  );
+};
