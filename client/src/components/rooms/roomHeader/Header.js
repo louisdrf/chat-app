0c9622c50e53858c4843bb2b1e7a9a063cdd34a8
Louis <louis.driffort@gmail.com>
2024-08-06 15:16:56 +0200

    add search message in room Input component

--- client/src/components/rooms/roomHeader/Header.js
+++ client/src/components/rooms/roomHeader/Header.js
@@ -1,22 +1,28 @@
 import React from 'react';
 import { Typography } from 'antd';
-import { UserAvatar } from '../../UserAvatar';
 import '../../../styles/components/roomHeader/room-header-container.scss'
 import { HeaderDropdownMenu } from './HeaderDropdownMenu';
 import { RoomAvatar } from '../../RoomAvatar';
+import { SearchMessage } from './SearchMessage';
 
 const { Title } = Typography;
 
 export const HeaderComponent = ({ room, roomName, onShowPinnedMessages, onShowMembersList }) => (
     <div className="headerContainer">
+
       <div className="headerContent">
         <RoomAvatar roomName={roomName} size={32} />
         <Title level={4} className="title">{roomName}</Title>
       </div>
-      <HeaderDropdownMenu 
-        onShowPinnedMessages={onShowPinnedMessages} 
-        onShowMembersList={onShowMembersList}
-        room={room}
-      />
+
+      <div className="headerContent">
+        <SearchMessage messages={room.messages}/>
+        <HeaderDropdownMenu 
+          onShowPinnedMessages={onShowPinnedMessages} 
+          onShowMembersList={onShowMembersList}
+          room={room}
+        />
+      </div>
+
     </div>
   )
---
+++ client/src/components/rooms/roomHeader/SearchMessage.js
@@ -0,0 +1,64 @@
+import React, { useState, useEffect, useRef } from 'react';
+import { Input, Dropdown, Menu } from 'antd';
+import Highlighter from 'react-highlight-words';
+
+export const SearchMessage = ({ messages }) => {
+  const [searchText, setSearchText] = useState('')
+  const [foundMessages, setFoundMessages] = useState([])
+  const [visible, setVisible] = useState(false)
+  const inputRef = useRef(null)
+
+  useEffect(() => {
+    if (searchText) {
+      const filtered = messages.filter((msg) =>
+        msg.content.toLowerCase().includes(searchText.toLowerCase())
+      );
+      setFoundMessages(filtered)
+      setVisible(true)
+    } else {
+      setFoundMessages([])
+      setVisible(false)
+    }
+  }, [searchText, messages])
+
+  const handleSearch = (e) => {
+    setSearchText(e.target.value)
+  }
+
+  const renderMenuItems = () => (
+    <Menu>
+      {foundMessages.length > 0 ? (
+        foundMessages.map((msg, index) => (
+          <Menu.Item key={index}>
+            <Highlighter
+              highlightClassName="search-highlight"
+              searchWords={[searchText]}
+              autoEscape
+              textToHighlight={msg.content}
+            />
+          </Menu.Item>
+        ))
+      ) : (
+        <Menu.Item>Aucun message trouvé</Menu.Item>
+      )}
+    </Menu>
+  )
+
+  return (
+    <div>
+      <Dropdown
+        overlay={renderMenuItems()}
+        trigger={['click']}
+        visible={visible}
+        onOpenChange={(flag) => setVisible(flag)}
+      >
+        <Input
+          placeholder="Chercher un message..."
+          onChange={handleSearch}
+          value={searchText}
+          ref={inputRef}
+        />
+      </Dropdown>
+    </div>
+  )
+}
